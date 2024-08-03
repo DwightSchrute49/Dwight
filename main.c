beba07a1206acdffd166460dcdace8a3fa829e64
DwightSchrute49 <159871146+DwightSchrute49@users.noreply.github.com>
2024-08-03 23:25:16 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void triplets(int arr[],int size);
 int main()
 {
-    myfunc();
+    int size,i;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    printf("Enter the elements of the array:");
+    int arr[size];
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Checking for triplets!!...\n");
+    triplets(arr,size);
 }
